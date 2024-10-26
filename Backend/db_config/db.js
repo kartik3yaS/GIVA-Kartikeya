58e505adc97a38682ff836b05a496debb0de8372
kartik3yaS <kartikeyashukla009@gmail.com>
2024-10-26 15:08:06 +0530

    databse pool connection

--- Backend/db_config/db.js
+++ Backend/db_config/db.js
@@ -3,14 +3,16 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString:
     "postgresql://productmanagement_p05a_user:d20jPJ2iuFybwGaKkHjdYujuWSTKgufs@dpg-cse9kirtq21c73874gog-a.oregon-postgres.render.com:5432/productmanagement_p05a",
-  ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false }, // Ensure SSL is properly configured for Render
 });
 
+// Test the connection
 pool.query("SELECT NOW()", (err, res) => {
   if (err) {
     console.error("Error connecting to the database:", err.stack);
   } else {
     console.log("Connected to the database successfully at:", res.rows[0].now);
   }
-  pool.end();
 });
+
+module.exports = pool;
