c26af7161725a155f7c628f1248f5eb3b7d0140c
kartik3yaS <kartikeyashukla009@gmail.com>
2024-10-26 22:23:50 +0530

    small comment add

--- Backend/db_config/db.js
+++ Backend/db_config/db.js
@@ -1,5 +1,8 @@
 const { Pool } = require("pg");
 
+// I tried hide the private data using the config .env file but some error was coming
+// So switch to direct string connection
+
 const pool = new Pool({
   connectionString:
     "postgresql://db_jraj_user:ZUYbgFp0vYU7UktfZ8CbIsaaopBkRmVC@dpg-csecvltsvqrc73f0sccg-a.oregon-postgres.render.com/db_jraj",
