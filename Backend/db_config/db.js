74c8e94a0f4000490b4c1b630006658c79c06fa6
kartik3yaS <kartikeyashukla009@gmail.com>
2024-10-26 14:34:05 +0530

    change in package.json

--- Backend/db_config/db.js
+++ Backend/db_config/db.js
@@ -1,15 +1,16 @@
-// src/db.js
 const { Pool } = require("pg");
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString:
+    "postgresql://productmanagement_p05a_user:d20jPJ2iuFybwGaKkHjdYujuWSTKgufs@dpg-cse9kirtq21c73874gog-a.oregon-postgres.render.com:5432/productmanagement_p05a",
+  ssl: { rejectUnauthorized: false },
 });
 
-module.exports = pool;
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.stack);
+  } else {
+    console.log("Connected to the database successfully at:", res.rows[0].now);
+  }
+  pool.end();
+});
